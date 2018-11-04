aa217bcb7b10ccaaf9fe689ddd2a0073e500ce23
sstienface <s.stien@fondationface.org>
2018-11-04 16:39:19 +0100

    Initial Commit

---
+++ exercice1.js
@@ -0,0 +1,26 @@
+/**
+ * Created by sstienface on 16/10/2018.
+ */
+
+
+// Inutile de modifier le code suivant
+var articles = [
+    "Orangina",
+    "Creme Fraiche",
+    "Tartiflette",
+    "Emmental",
+    "Bananes",
+    "Chips",
+    "Bieres",
+    "Pizza"
+];
+
+// Votre code à partir d'ici :
+
+
+
+
+
+
+
+
